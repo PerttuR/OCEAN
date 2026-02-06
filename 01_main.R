0ad26690b9fac3913d9ddb32f63c9e8560f53363
Perttu Rantanen <perttu.rantanen@luke.fi>
2026-02-06 16:18:42 +0200

    repo structure

---
+++ 01_main.R
@@ -0,0 +1,7 @@
+
+
+# PATHS ####
+path <- paste0(getwd(), "/") # Working directory
+codePath  <- paste0(path, "prog/")   # Location to store R scripts
+outPath   <- paste0(path, paste0("out/"))   # Location to store the results
+dataPath  <- paste0(path, "orig/")      # Location to store tacsat (VMS) and eflalo (logbook) data
---
+++ prog/01_main.R
@@ -0,0 +1,6 @@
+
+
+# PATHS ####
+path <- paste0(getwd(), "/") # Working directory
+codePath  <- paste0(path, "prog/")   # Location to store R scripts
+outPath   <- paste0(path, paste0("out/", run.year, "/"))   # Location to store the results
